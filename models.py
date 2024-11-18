da7f7b5f80a6a64c515bccb4f768cf2a5747d673
Tetyana Pavlyuk <tetyana.pas@gmail.com>
2024-11-18 14:42:58 +0200

    Fix models.py in compliance flake8

--- models.py
+++ models.py
@@ -1,4 +1,6 @@
-from sqlalchemy import Column, Integer, String, Date, ForeignKey, UniqueConstraint
+from sqlalchemy import (
+    Column, Integer, String, Date, ForeignKey, UniqueConstraint
+)
 from sqlalchemy.orm import relationship
 
 from database import Base
